5b227760e0a1feb4b18b4ea360f2d0938ffed873
Anderson Lima <anderson_developer@outlook.com>
2022-09-20 18:50:23 -0300

    feat - adding adding route and question screen | adding bootstrap | adding styles.css.

--- index.js
+++ index.js
@@ -4,9 +4,14 @@ const app = express();
 // Usar o EJS como engine...
 
 app.set("view engine", "ejs");
+app.use(express.static("public"));
 
 app.get("/", (req, res) => {
   res.render("index");
+});
+
+app.get("/perguntar", (req, res) => {
+  res.render("perguntar");
 });
 
 app.listen(8080, () => {
