75a85b560683dc9fa64a03d84d936bf2503204b0
Anderson Lima <anderson_developer@outlook.com>
2022-09-21 18:48:56 -0300

    fix - adding body parser | finished question screen.

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
 const express = require("express");
 const app = express();
+const bodyParser = require("body-parser");
 
 // Usar o EJS como engine...
 
 app.set("view engine", "ejs");
 app.use(express.static("public"));
+
+// Body Parser
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 app.get("/", (req, res) => {
   res.render("index");
@@ -12,6 +18,14 @@ app.get("/", (req, res) => {
 
 app.get("/perguntar", (req, res) => {
   res.render("perguntar");
+});
+
+app.post("/salvarpergunta", (req, res) => {
+  var titulo = req.body.titulo;
+  var descricao = req.body.descricao;
+  res.send(
+    "Formulário recebido! titulo " + titulo + " " + " descricao " + descricao
+  );
 });
 
 app.listen(8080, () => {
