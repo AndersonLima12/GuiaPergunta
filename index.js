d4deb850aa85c3af755f3ba53630b92f1b21f405
Anderson Lima <anderson_developer@outlook.com>
2022-10-01 11:33:21 -0300

    feat: ordering of questions sequelize.

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ app.use(bodyParser.json());
 // Rotas
 
 app.get("/", (req, res) => {
-  Pergunta.findAll({ raw: true }).then((perguntas) => {
+  Pergunta.findAll({ raw: true, order: [["id", "DESC"]] }).then((perguntas) => {
     res.render("index", {
       perguntas: perguntas,
     });
