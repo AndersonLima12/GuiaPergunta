1017f4d8cf864d8ec7acdb646679eaaaa2bad694
Anderson Lima <anderson_developer@outlook.com>
2022-10-03 00:17:37 -0300

    feat: adicionando model | rotas | template.

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const app = express();
 const bodyParser = require("body-parser");
 const connection = require("./data/database");
 const Pergunta = require("./data/Pergunta");
+const Resposta = require("./data/Resposta");
 
 // Database
 connection
@@ -60,6 +61,17 @@ app.get("/pergunta/:id", (req, res) => {
     } else {
       res.redirect("/"); // Não encontrada
     }
+  });
+});
+
+app.post("/responder", (req, res) => {
+  var corpo = req.body.corpo;
+  var perguntaId = req.body.pergunta;
+  Resposta.create({
+    corpo: corpo,
+    perguntaId: perguntaId,
+  }).then(() => {
+    res.redirect("/pergunta/" + perguntaId);
   });
 });
 
